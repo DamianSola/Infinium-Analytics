52ddc58cbafcdee7ee346bed9ae3c767e63c99ff
damian sola <damiansola99@gmail.com>
2023-12-02 14:40:53 -0300

    trabajando en el responsive

--- src/Components/Portfolio/Carousel.jsx
+++ src/Components/Portfolio/Carousel.jsx
@@ -16,12 +16,18 @@ const Carousel = () => {
 
     const settings = {
         dots: true,
+        number: true,
         lazyLoad: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        initialSlide: 1
+        initialSlide: 0,
+        customPaging: (i) => (
+          <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" , fontSize: "17px"}}>
+            {i + 1}
+          </div>
+        ),
      
     };
     
@@ -29,16 +35,16 @@ const Carousel = () => {
       <div>
         <SliderCarousel {...settings}>
         <div>
-            <h4>Finanzas</h4>
-            <iframe title="Sample Finanzas"width="80%" height="420" src="https://app.powerbi.com/view?r=eyJrIjoiM2MwNGZkNjItMDRkMS00ZGNhLWJjYmEtZGI2NTVmM2FiZjMwIiwidCI6IjNlMDUxM2Q2LTY4ZmEtNDE2ZS04ZGUxLTZjNWNkYzMxOWZmYSIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+          <h4>Finanzas</h4>
+          <iframe title="Sample Finanzas"width="80%" height="420" src="https://app.powerbi.com/view?r=eyJrIjoiM2MwNGZkNjItMDRkMS00ZGNhLWJjYmEtZGI2NTVmM2FiZjMwIiwidCI6IjNlMDUxM2Q2LTY4ZmEtNDE2ZS04ZGUxLTZjNWNkYzMxOWZmYSIsImMiOjR9" frameborder="0" allowFullScreen="true"/>
         </div>
         <div>
-            <h4>Recursos humanos</h4>
-            <iframe title="HR Sample" width="80%" height="420" src="https://app.powerbi.com/view?r=eyJrIjoiNzQ0NmNhOGItOGJmYy00MDYyLTk0ODItZDgxMDUzZDNmNmE0IiwidCI6IjNlMDUxM2Q2LTY4ZmEtNDE2ZS04ZGUxLTZjNWNkYzMxOWZmYSIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+          <h4>Recursos humanos</h4>
+          <iframe title="HR Sample" width="80%" height="420" src="https://app.powerbi.com/view?r=eyJrIjoiNzQ0NmNhOGItOGJmYy00MDYyLTk0ODItZDgxMDUzZDNmNmE0IiwidCI6IjNlMDUxM2Q2LTY4ZmEtNDE2ZS04ZGUxLTZjNWNkYzMxOWZmYSIsImMiOjR9" frameborder="0" allowFullScreen="true"/>
         </div>
         <div>
-            <h4>Inventarios</h4>
-            <iframe title="Report Section" width="80%" height="420" src="https://app.powerbi.com/view?r=eyJrIjoiODI5NTM0YTAtOTFjMi00NjdhLWFiMzMtZGYwZDc4MTE0MGJhIiwidCI6IjNlMDUxM2Q2LTY4ZmEtNDE2ZS04ZGUxLTZjNWNkYzMxOWZmYSIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+          <h4>Inventarios</h4>
+          <iframe title="Report Section" width="80%" height="420" src="https://app.powerbi.com/view?r=eyJrIjoiODI5NTM0YTAtOTFjMi00NjdhLWFiMzMtZGYwZDc4MTE0MGJhIiwidCI6IjNlMDUxM2Q2LTY4ZmEtNDE2ZS04ZGUxLTZjNWNkYzMxOWZmYSIsImMiOjR9" frameborder="0" allowFullScreen="true"/>
         </div>
         </SliderCarousel>
       </div>
