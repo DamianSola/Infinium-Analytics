64c29fc62f7c5aec3c977d3f6d2f74e36070a46a
damian sola <damiansola99@gmail.com>
2023-12-03 16:53:42 -0300

    app lista

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="es">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="icon" href="favicon.ico" type="image/x-icon"></link>
+        <link rel="icon" href="/infinium.svg" type="image/x-icon"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
