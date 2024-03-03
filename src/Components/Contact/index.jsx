9eb29ccb1a1cbdce7c42c59330f2e3096db9191f
damian sola <damiansola99@gmail.com>
2024-03-03 17:06:25 -0300

    arreglar el formulario

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -2,24 +2,52 @@
 import style from "./style.module.css"
 import emailjs from '@emailjs/browser';
 import Image from "next/image";
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import foto from "src/Components/Contact/picontact.png"
 
 const Contact = () => {
 
-    const form = useRef()
+    // const form = useRef()
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        company: '',
+        phone:'',
+        message: ''
+      });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+    
 
     const sendEmail = (e) => {
         e.preventDefault();
-        emailjs.sendForm('service_i0g2eus', 'template_pkjvyor', form.current, "a_mj9GG3z_rUB1m7F")
+
+        emailjs.send("service_ydywbmc","template_pkjvyor",{
+            name: formData.name,
+            company: formData.company, 
+            phone: formData.phone,
+            message: formData.message,
+            email: formData.email,
+            },"a_mj9GG3z_rUB1m7F"
+        )
         .then((result) => {
-            console.log(result.text);
+            // console.log(result);
             alert("el mensaje se envió correctamente")
         }, (error) => {
-            console.log(error.text);
+            // console.log(error.text);
             alert("algo salio mal. intente de nuevo")
         });
-        form.current.reset()
+        setFormData({
+            name: '',
+            email: '', 
+            company: '',
+            phone:'',
+            message: ''
+        })
     }
 
     return(
@@ -31,13 +59,54 @@ const Contact = () => {
                     <p>Podés contactarnos completando el siguente formulario</p>
                     <Image src={foto} className={style.foto} width="180" alt="contact-img"/>
                 </section>
-                <form className={style.form} ref={form} onSubmit={sendEmail}>
+                <form className={style.form} onSubmit={sendEmail}>
 
-                    <input type="text" id="name" placeholder="Nombre*" required/><br/>
-                    <input type="email" id="email" placeholder="Email*" required/><br/>
-                    <input type="text" id="phone" placeholder="Teléfono" /><br/>
-                    <input type="text" id="company" placeholder="Companía*" required/><br/>
-                    <textarea type="text" id="message" placeholder="Consulta*" rows="7" required/><br/>
+                    <input
+                        type="text" 
+                        id="name"
+                        name="name" 
+                        onChange={handleChange}
+                        value={formData.name}
+                        placeholder="Nombre*" 
+                        required/>
+                    <br/>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        onChange={handleChange}
+                        value={formData.email}
+                        id="email" 
+                        placeholder="Email*" 
+                        required/>
+                    <br/>
+                    <input 
+                        type="text" 
+                        name="phone" 
+                        id="phone" 
+                        onChange={handleChange}
+                        value={formData.phone}
+                        placeholder="Teléfono" 
+                        />
+                    <br/>
+                    <input 
+                        name="company" 
+                        onChange={handleChange}
+                        value={formData.company}
+                        type="text" 
+                        id="company" 
+                        placeholder="Companía*" 
+                        required/>
+                    <br/>
+                    <textarea 
+                        name="message" 
+                        type="text" 
+                        id="message" 
+                        onChange={handleChange}
+                        value={formData.message}
+                        placeholder="Consulta*" 
+                        rows="7" 
+                        required/>
+                    <br/>
 
                     <button type="submit">Enviar</button>
                 </form>
